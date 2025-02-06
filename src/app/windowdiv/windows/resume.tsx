24a9167024a5bdd8763187024165b679a92448f5
scaboodles <owenj.wolff2@gmail.com>
2025-02-06 12:25:19 -0500

    minor css bug corrected

--- src/app/windowdiv/windows/resume.tsx
+++ src/app/windowdiv/windows/resume.tsx
@@ -22,7 +22,7 @@ export const ResumeGuts = () => {
                 <a href="/images/OwenWolffResumeFall2024.pdf" target="_blank" rel="noopener noreferrer" className='fakeButton clickable' >View in Browser</a>
             </div>
 
-            <div style={{ display: 'flex', justifyContent: 'center', margin: '20px 0', backgroundColor:'white'}}>
+            <div style={{ display: 'flex', justifyContent: 'center', padding: '20px 0', backgroundColor:'white'}}>
                 <Document file={"/images/OwenWolffResumeFall2024.pdf"} onLoadError={console.error} >
                     <Page pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false}/>
                 </Document>
